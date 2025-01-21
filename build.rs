164cff65b74bf21c74f6e444e93f04ea9322b32a
Mykyta Sikachyna <sikachyna.n@gmail.com>
2025-01-21 16:37:35 +0200

    feat: change .exe file details

---
+++ build.rs
@@ -0,0 +1,13 @@
+extern crate winres;
+extern crate winapi;
+
+fn main( ) {
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_language(winapi::um::winnt::MAKELANGID(
+            winapi::um::winnt::LANG_ENGLISH,
+            winapi::um::winnt::SUBLANG_ENGLISH_US
+        ));
+        res.compile().unwrap();
+    }
+}
